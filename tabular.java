d28bddb26bfd58d4de90920b0f468130d54d126b
Rangaswamy-07 <rockyrangaswamy@gmail.com>
2025-06-12 15:27:37 +0530

    Create tabular.java

---
+++ tabular.java
@@ -0,0 +1,18 @@
+
+public class tabular
+{
+   
+    
+	public static void main(String[] args){
+	    int n =6;
+	    int [] memo = new int[n+1];
+	    
+	        memo[1]=1;
+	        for(int i=2;i<memo.length;i++){
+	            memo[i]=memo[i-1]+memo[i-2];
+	        }
+	    
+	    System.out.println(memo[n]);;
+	    
+	}
+}
